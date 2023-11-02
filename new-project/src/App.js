e646a2ed19323eac2ad8b71addb5208409831c0c
Leonardohas <leonardohas83@gmail.com>
2023-11-02 16:52:09 -0300

    jsx class

--- new-project/src/App.js
+++ new-project/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Leonardo';
+  const neeName = name.toUpperCase();
+
+  function sum(a,b) {
+    return a + b;
+  };
+
+  const url = 'https://via.placeholder.com/150';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {neeName}</p>
+      <p>soma: {sum(2 , 2)}</p>
+      <img src={url} alt='Imagem' />
     </div>
   );
 }
