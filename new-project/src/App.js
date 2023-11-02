af56d54d2514bc390b242b8e5eb80847c4f0b1e7
Leonardohas <leonardohas83@gmail.com>
2023-11-02 18:06:00 -0300

    using props

--- new-project/src/App.js
+++ new-project/src/App.js
@@ -1,23 +1,18 @@
 import './App.css';
-import FirstComponents from './components/FirstComponents'
+import SayMyName from './components/SayMyName'
+import Person from './components/Person'
 
 function App() {
-  const name = 'Leonardo';
-  const neeName = name.toUpperCase();
-
-  function sum(a,b) {
-    return a + b;
-  };
-
-  const url = 'https://via.placeholder.com/150';
-
+  const name = "Silva"
   return (
     <div className="App">
-      <h2>Alterando o JSX</h2>
-      <p>Olá, {neeName}</p>
-      <p>soma: {sum(2 , 2)}</p>
-      <img src={url} alt='Imagem' />
-      <FirstComponents />
+      <SayMyName name="Leonardo" />
+      <SayMyName name={name} />
+      <Person 
+        name={name}
+        age='28'
+        profession='Developer'
+      />
     </div>
   );
 }
