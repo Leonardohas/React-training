015a5194ad53b5a75b7555c550e1ab630f3f23be
Leonardohas <leonardohas83@gmail.com>
2023-11-06 13:40:00 -0300

    learning about events in react

--- new-project/src/App.js
+++ new-project/src/App.js
@@ -3,21 +3,17 @@ import SayMyName from './components/SayMyName'
 import Person from './components/Person'
 import Phrase from './components/Phrase'
 import List from './components/List'
+import Event from './components/Event'
+import Form from './components/Form'
 
 function App() {
   const name = "Silva"
   return (
     <div className="App">
-      <SayMyName name="Leonardo" />
-      <SayMyName name={name} />
-      <Person 
-        name={name}
-        age='28'
-        profession='Developer'
-      />
-      <Phrase />
-      <Phrase />
-      <List />
+      <h1>Testando eventos</h1>
+      <Event number='1' />
+      <Event number='2' />
+      <Form />
     </div>
   );
 }
