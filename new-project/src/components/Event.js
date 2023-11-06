4d4b02258d2afcb492a08a56ab383a79b03736bb
Leonardohas <leonardohas83@gmail.com>
2023-11-06 14:51:27 -0300

    learning about methods by props

--- new-project/src/components/Event.js
+++ new-project/src/components/Event.js
@@ -1,15 +1,22 @@
+import Button from './eventsComponents/Button'
+
 function Event({number}) {
 
     function myEvent() {
-        console.log(`Fui ativado ${number}`);
-    }
+        console.log(`Ativando primeiro evento!!!!`);
+    };
+
+    function secondEvent() {
+        console.log('Ativando o segundo evento!!!!');
+    };
 
     return(
         <div>
             <p>Clique para disparar um evento:</p>
-            <button onClick={myEvent}> Ativar </button>
+            <Button event={myEvent} text="Fist Event" />
+            <Button event={secondEvent} text="second Event" />
         </div>
     )
-}
+}; 
 
 export default Event
