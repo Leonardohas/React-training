218dd276452451cab468b01bab8212a2eb54571b
Leonardohas <leonardohas83@gmail.com>
2023-11-08 12:15:22 -0300

    learning about react icons

--- new-project/src/components/layout/Foter.js
+++ new-project/src/components/layout/Foter.js
@@ -1,5 +1,17 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import styles from './Foter.module.css'
+
 function Foter() {
-    return <footer></footer>
+    return(
+        <footer>
+            <ul className={styles.social_list}>
+                <li><FaFacebook/></li>
+                <li><FaInstagram/></li>
+                <li><FaLinkedin/></li>
+            </ul>
+            <p>Rodapé</p>
+        </footer>
+    )
 };
 
 export default Foter
