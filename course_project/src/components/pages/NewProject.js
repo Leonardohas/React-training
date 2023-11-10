5ac7cb4dd664d22b7c90688c987641526712ad74
Leonardohas <leonardohas83@gmail.com>
2023-11-10 12:27:46 -0300

    created new project form component and styles

--- course_project/src/components/pages/NewProject.js
+++ course_project/src/components/pages/NewProject.js
@@ -1,5 +1,14 @@
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
+
 function NewProject() {
-    return <h1>NewProject</h1>
+    return (
+        <div className={styles.new_project_container}>
+            <h1>Create your project</h1>
+            <p>Create your project and then add your services</p>
+            <ProjectForm buttonText='Create project'/>
+        </div>
+    )
 };
 
 export default NewProject
